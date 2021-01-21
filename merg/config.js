e75a99ec0a508987998dea24b88abaecfb2c999b
zhe4ka <63822712+zhe4ka@users.noreply.github.com>
2021-01-20 23:40:30 -0500

    adding files

---
+++ merg/config.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGODB:'mongodb+srv://classed:amzDx8eBmrQA3jOb@evgeniya.mgq7a.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
---
+++ merg/models/post.js
@@ -0,0 +1,23 @@
+const{model, Schema}=require('mongoose');
+const postSchema= new Schema({
+    username: String,
+    body: String,
+    createdAt: String,
+    comments:[
+       { username: String,
+        body: String,
+        createdAt: String,
+    }
+    ],
+    likes: [ {
+        username: String,
+        createdAt: String
+    }],
+    user: {
+        type:Schema.Types.ObjectId,
+        ref: 'users'
+    }
+})
+
+module.exports = model('Post', postSchema);
+
